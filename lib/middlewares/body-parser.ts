282936426502219a9367238ce73623b6f86e3212
Lucca <luccazvps@gmail.com>
2023-11-17 18:17:01 -0300

    feat: Update body parser middleware

--- lib/middlewares/body-parser.ts
+++ lib/middlewares/body-parser.ts
@@ -1,10 +1,22 @@
+import { Request } from "../types";
+const BODY_LENGTH = 1024 * 1024
 export const bodyParser = () => {
-  return async (req, res, fn) => {
+  return async (req:Request, res, fn) => {
+    if (req.headers["content-type"] !== "application/json") {
+      fn()
+      return 
+    }
+    let  contentLength = parseInt(req.headers['content-length'], 10);
+    contentLength = isNaN(contentLength) ? contentLength : 1
+    if (contentLength > BODY_LENGTH) {
+      res.send(413,"Body too large");
+      return;
+    }
     const bodyParserFn = new Promise((resolve, reject) => {
       let body = Buffer.from("");
 
       req.on("data", (chunk) => {
-        if (Buffer.length + chunk.length > 1024 * 1024) {
+        if (Buffer.length + chunk.length > BODY_LENGTH) {
           reject(new Error("Body too large"));
           res.writeHead(413);
           res.end("Body too large");
@@ -18,7 +30,8 @@ export const bodyParser = () => {
         try {
           if (body.length > 0) {
             body = JSON.parse(body.toString("utf-8"));
-          } else {
+          }
+          else {
             body = {} as Buffer;
           }
           req["body"] = body;
